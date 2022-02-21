11c88c78d5364e6a6d823b6d5110fc3774a892de
Young Soo Koh <kohys92@gmail.com>
2022-02-21 16:27:27 +0900

    스프링 입문 시작

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,23 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!");
+        return "hello";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, @RequestParam(value = "another") String another, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("another", another);
+        return "hello-template";
+    }
+}
